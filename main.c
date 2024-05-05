af6ab529c11e867fdd025520052df73f3b8bd0fa
lordnic99 <nguyenluongdinhhoang@gmail.com>
2024-05-05 15:28:54 +0700

    raw mode setup ok

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <asm-generic/errno-base.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termio.h>
 #include <termios.h>
@@ -5,23 +9,56 @@
 
 struct termios org_terminal;
 
-void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_terminal); }
+void die(const char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_terminal) == -1) {
+    die("disable raw");
+  }
+}
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &org_terminal);
+  if (tcgetattr(STDIN_FILENO, &org_terminal) == -1) {
+    die("enable raw mode");
+  }
 
   struct termios raw = org_terminal;
   atexit(disableRawMode);
-  raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("enable raw mode");
+  }
 }
 
 int main() {
-  tcgetattr(STDIN_FILENO, &org_terminal);
+  if (tcgetattr(STDIN_FILENO, &org_terminal) == -1) {
+    die("get tc");
+  }
+
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (1) {
+    c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+      die("read");
+    }
 
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') {
+      break;
+    }
+  }
   return 0;
 }
